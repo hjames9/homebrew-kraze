dd85a157dfab779828de1ca0cec8319002f0242b
Hayden A. James <hayden.james@gmail.com>
2026-05-30 01:28:31 -0400

    Upgrade kraze to 0.8.5

--- Formula/kraze.rb
+++ Formula/kraze.rb
@@ -1,18 +1,18 @@
 class Kraze < Formula
   desc "Kubernetes development environment manager with docker-compose-like experience"
   homepage "https://github.com/hjames9/kraze"
-  version "0.8.4"
+  version "0.8.5"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/hjames9/kraze/releases/download/v#{version}/kraze-v#{version}-darwin-arm64"
-      sha256 "1ab1e546ccba5814d4e2416e98b93ea45e0734a3120a0071169d1fdaffba7143"
+      sha256 "2e1e4a3f6b928a1ca1831f4a405c7e2f7c6f7d121807dba55ddab0108522547e"
     end
 
     on_intel do
       url "https://github.com/hjames9/kraze/releases/download/v#{version}/kraze-v#{version}-darwin-amd64"
-      sha256 "432cfb4f6c3050cfa50d868aed9007f2f22237a72d4d862ba9b0e5f14f079516"
+      sha256 "32a62a8b0b2cd9116ba376231b2eac4907b626f0f2ed24501a21d4fa9ac2436b"
     end
   end
 
